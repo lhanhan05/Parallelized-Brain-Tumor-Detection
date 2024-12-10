a72d36d9b1c4c1cd54c185d4ce1d64a91b744041
Jeffrey Paulraj <43931770+jeffreypaulraj@users.noreply.github.com>
2024-12-10 17:44:02 -0500

    updates to loss

--- modules/SoftMaxCrossEntropyLoss.cpp
+++ modules/SoftMaxCrossEntropyLoss.cpp
@@ -10,8 +10,10 @@ float SoftMaxCrossEntropyLoss::forward(const Tensor<float, 2>& logits, const Ten
 
     // Compute softmax
     Tensor<float, 2> exp_logits = logits_transposed.exp();
-    Tensor<float, 1> expsum = exp_logits.sum(0); // Sum along columns (axis=0)
-    softmax = exp_logits / expsum.broadcast(logits_transposed.dimensions()[0]); // Normalize along columns
+    array<int, 1> dims = {0};
+    Tensor<float, 1> expsum = exp_logits.sum(dims); // Sum along columns (axis=0)
+    array<int, 2> logit_dims = {(int)exp_logits.dimension(0), 1};
+    softmax = exp_logits / expsum.broadcast(logit_dims);
 
     // Compute log(softmax)
     Tensor<float, 2> log_softmax = softmax.log();
@@ -27,47 +29,48 @@ float SoftMaxCrossEntropyLoss::forward(const Tensor<float, 2>& logits, const Ten
     }
 
     // Predicted class labels (for computing contrastive loss)
-    preds = softmax.argmax(0); 
+    // preds = softmax.argmax(0); 
+    return loss_sum;
 
     // Split predictions into 3 parts for contrastive loss
-    int length = preds.dimensions()[0];
-    int split_size = length / 3;
+    // int length = preds.dimensions()[0];
+    // int split_size = length / 3;
 
-    std::vector<Tensor<int, 1>> pred_parts;
-    pred_parts.push_back(preds.slice(array<Index, 1>{0}, array<Index, 1>{split_size}));
-    pred_parts.push_back(preds.slice(array<Index, 1>{split_size}, array<Index, 1>{split_size}));
-    pred_parts.push_back(preds.slice(array<Index, 1>{2 * split_size}, array<Index, 1>{length - 2 * split_size}));
+    // std::vector<Tensor<int, 1>> pred_parts;
+    // pred_parts.push_back(preds.slice(array<Index, 1>{0}, array<Index, 1>{split_size}));
+    // pred_parts.push_back(preds.slice(array<Index, 1>{split_size}, array<Index, 1>{split_size}));
+    // pred_parts.push_back(preds.slice(array<Index, 1>{2 * split_size}, array<Index, 1>{length - 2 * split_size}));
 
-    std::vector<int> sims(length, 0);
-    for (int i = 0; i < length; ++i) {
-        std::unordered_map<int, int> element_count;
-        for (auto& part : pred_parts) {
-            int element = part(i);
-            element_count[element]++;
-        }
-        sims[i] = std::max_element(element_count.begin(), element_count.end(),
-            [](const std::pair<int, int>& a, const std::pair<int, int>& b) {
-                return a.second < b.second;
-            })->second;
-    }
+    // std::vector<int> sims(length, 0);
+    // for (int i = 0; i < length; ++i) {
+    //     std::unordered_map<int, int> element_count;
+    //     for (auto& part : pred_parts) {
+    //         int element = part(i);
+    //         element_count[element]++;
+    //     }
+    //     sims[i] = std::max_element(element_count.begin(), element_count.end(),
+    //         [](const std::pair<int, int>& a, const std::pair<int, int>& b) {
+    //             return a.second < b.second;
+    //         })->second;
+    // }
 
-    TensorMap<Tensor<int, 1>> contrast_loss_section(sims.data(), sims.size());
-    contrast_loss_section = contrast_loss_section.cast<float>();
-    contrast_loss_section = contrast_loss_section * -0.01f;
-    int repeat_count = 3;
-    Tensor<float, 1> contrast_loss(contrast_loss_section.dimension(0)*repeat_count);
+    // TensorMap<Tensor<int, 1>> contrast_loss_section_int(sims.data(), sims.size());
+    // Tensor<float, 1> contrast_loss_section = contrast_loss_section_int.cast<float>();
+    // contrast_loss_section = contrast_loss_section * -0.01f;
+    // int repeat_count = 3;
+    // Tensor<float, 2> contrast_loss(contrast_loss_section.dimension(0)*repeat_count, 1);
     
-    float contrast_sum = 0;
-    for (int i = 0; i < repeat_count; ++i) {
-        for (int j = 0; j < contrast_loss_section.dimension(0); ++j) {
-            contrast_loss(i * contrast_loss_section.dimension(0) + j) = contrast_loss_section(j);
-            contrast_sum += contrast_loss_section(j);
-        }
-    }
+    // float contrast_sum = 0;
+    // for (int i = 0; i < repeat_count; ++i) {
+    //     for (int j = 0; j < contrast_loss_section.dimension(0); ++j) {
+    //         contrast_loss(i * contrast_loss_section.dimension(0) + j, 0) = contrast_loss_section(j);
+    //         contrast_sum += contrast_loss_section(j);
+    //     }
+    // }
 
-    contrast_loss = contrast_loss.reshape(DSizes<int, 1>(labels.dimension(0), 1)); 
-    contrast = contrast_loss * labels;
-    return 0.8f * loss_sum + 0.2f * contrast_sum;
+    // contrast_loss = contrast_loss.reshape(DSizes<int, 1>(labels.dimension(0), 1)); 
+    // contrast = contrast_loss * labels;
+    // return 0.8f * loss_sum + 0.2f * contrast_sum;
 }
 
 Tensor<int, 1> SoftMaxCrossEntropyLoss::getPreds(){
@@ -79,12 +82,6 @@ Tensor<float, 2> SoftMaxCrossEntropyLoss::backward() {
     return 0.8f * softmax_back + 0.2f * contrast;
 }
 
-float SoftMaxCrossEntropyLoss::getAccu() {
-    Tensor<int, 1> preds = softmax.argmax(0);
-    Tensor<int, 1> actuals = labels.argmax(0);
-    int correctcount = getCorrectCount(preds, actuals);
-    return static_cast<float>(correctcount) / preds.dimensions()[0];
-}
 
 int getCorrectCount(Tensor<int, 1> preds, Tensor<int, 1> actuals){
     int count = 0;
@@ -94,4 +91,10 @@ int getCorrectCount(Tensor<int, 1> preds, Tensor<int, 1> actuals){
         }
     }
     return count;
+}
+
+float SoftMaxCrossEntropyLoss::getAccu() {
+    Tensor<int, 1> actuals = labels.argmax(0);
+    int correctcount = getCorrectCount(preds, actuals);
+    return static_cast<float>(correctcount) / preds.dimensions()[0];
 }
