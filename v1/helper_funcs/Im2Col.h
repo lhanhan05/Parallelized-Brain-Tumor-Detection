6e1d81d69ea518355993cc05e174daaae0f66be6
lhanhan05 <103400250+lhanhan05@users.noreply.github.com>
2024-12-13 19:15:14 -0500

    Delete v1 directory

--- v1/helper_funcs/Im2Col.h
+++
@@ -1,8 +0,0 @@
-#include <Eigen/Dense>
-#include <unsupported/Eigen/CXX11/Tensor>
-using namespace Eigen;
-
-namespace Im2Col {
-    Tensor<float, 2> im2col(const Tensor<float, 4>& X, int k_height, int k_width, int padding, int stride);
-    Tensor<float, 4> im2col_bw(Tensor<float, 2>& grad_X_col, Eigen::array<int, 4>& X_shape, int k_height, int k_width, int padding, int stride);
-} 
